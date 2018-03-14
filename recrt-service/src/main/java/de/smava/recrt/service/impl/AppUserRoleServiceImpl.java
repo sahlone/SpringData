9af9ab8d95e37e2ff97743176c00dcfe4cacb821
Sahil Lone <sahill@cakesolutions.net>
2018-03-14 00:05:15 +0000

    add cache to apiUser

--- recrt-service/src/main/java/de/smava/recrt/service/impl/AppUserRoleServiceImpl.java
+++ recrt-service/src/main/java/de/smava/recrt/service/impl/AppUserRoleServiceImpl.java
@@ -7,6 +7,8 @@ import de.smava.recrt.persistence.model.AppUserEntity;
 import de.smava.recrt.persistence.repository.AppUserRoleRepository;
 import de.smava.recrt.service.AppUserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +22,7 @@ public class AppUserRoleServiceImpl implements AppUserRoleService {
 
     @Override
     @Transactional
+    @Cacheable(value = "appUsers",key = "appUser.username")
     public List<? extends AppUserRole> getByAppUser(AppUser appUser) throws RecrtServiceException {
         return repository.findByKeyAppUser((AppUserEntity) appUser);
     }
